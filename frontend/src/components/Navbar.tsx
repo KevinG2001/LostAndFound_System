bda9d0653dd5e75c23d942e18b654ac6f585003d
Kevin <kevinglennon01@gmail.com>
2024-09-29 12:47:49 +0100

    Landing page being worked on

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -4,13 +4,15 @@ function Navbar() {
   return (
     <>
       <div className={Styles.navContainer}>
-        <div className={Styles.navHeader}>Header</div>
-        <div className={Styles.navLinks}>
-          <div>Dashboard</div>
-          <div>Items</div>
-          <div>Tickets</div>
+        <div className={Styles.navWrapper}>
+          <div className={Styles.navHeader}>Header</div>
+          <div className={Styles.navLinks}>
+            <div>Dashboard</div>
+            <div>Items</div>
+            <div>Tickets</div>
+          </div>
+          <div className={Styles.navFooter}>Footer</div>
         </div>
-        <div className={Styles.navFooter}>Footer</div>
       </div>
     </>
   );
