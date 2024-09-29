ccaac23b7d6dae3b509629743aff3c34a98b53ef
Kevin <kevinglennon01@gmail.com>
2024-09-29 22:44:32 +0100

    Item table being setup

--- frontend/src/pages/Items.tsx
+++ frontend/src/pages/Items.tsx
@@ -1,7 +1,32 @@
+import Styles from "../styles/pages/itemsPage.module.scss";
+
 function Items() {
   return (
     <>
-      <div>Items</div>
+      <div className={Styles.itemsContainer}>
+        <div className={Styles.statsWrapper}>
+          <div className={Styles.statBubble}>Items Lost</div>
+          <div className={Styles.statBubble}>Items Returned</div>
+          <div className={Styles.statBubble}>Items Expiring</div>
+          <div className={Styles.statBubble}>Last stat</div>
+        </div>
+        <div className={Styles.itemTable}>
+          <table>
+            <tr>
+              <th>ID</th>
+              <th>Name</th>
+              <th>Description</th>
+              <th>Group</th>
+              <th>Brand</th>
+              <th>Route</th>
+              <th>Garage</th>
+            </tr>
+            <tr>
+              <th>1</th>
+            </tr>
+          </table>
+        </div>
+      </div>
     </>
   );
 }
