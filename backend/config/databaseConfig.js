e02d2eee909b08af24b34e51057a76cb82e4c7a7
Kevin <kevinglennon01@gmail.com>
2024-10-03 18:54:46 +0100

    Changed database config to just work with local db

--- backend/config/databaseConfig.js
+++ backend/config/databaseConfig.js
@@ -1,19 +1,7 @@
-const mongoose = require("mongoose");
 const dotenv = require("dotenv");
+const mongoose = require("mongoose");
 
 dotenv.config();
-
-const connectDB = async () => {
-  try {
-    await mongoose.connect(process.env.MONGODB_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
-    console.log("MongoDB connected");
-  } catch (error) {
-    console.error("MongoDB connection error: ", error.message);
-    process.exit(1);
-  }
-};
+const uri = process.env.MONGO_URI;
 
 module.exports = connectDB;
