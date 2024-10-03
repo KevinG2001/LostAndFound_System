e9024d361a7207a4f88da182f3138d52c2d3d841
Kevin <kevinglennon01@gmail.com>
2024-10-03 19:17:44 +0100

    Custom hook for itemList

--- frontend/src/pages/Items.tsx
+++ frontend/src/pages/Items.tsx
@@ -1,11 +1,11 @@
-import { useEffect, useState } from "react";
 import Styles from "../styles/pages/itemsPage.module.scss";
 import TableView from "../components/Views/TableView";
 import useItemCount from "../util/useItemCount";
+import useItemList from "../util/useItemList";
 
 function Items() {
-  const [items, setItems] = useState([]);
-  const { itemCounts } = useItemCount("count"); // Assuming itemCounts is an object
+  const { itemCounts } = useItemCount("count");
+  const { items } = useItemList("list");
 
   //Columns pased into the TableView
   const columns = [
@@ -19,30 +19,12 @@ function Items() {
     { header: "Status", accessor: "status" },
   ];
 
-  useEffect(() => {
-    const fetchItems = async () => {
-      try {
-        const response = await fetch("http://localhost:4000/items/list");
-        if (!response.ok) {
-          throw new Error("Failed to fetch items");
-        }
-        const data = await response.json();
-        setItems(data);
-      } catch (error) {
-        console.error("Error fetching items:", error);
-      }
-    };
-
-    fetchItems();
-  }, []);
-
   return (
     <>
       <div className={Styles.itemsContainer}>
         <div className={Styles.statsWrapper}>
           <div className={Styles.statBubble}>
             Total Items Lost <br />
-            {/* Make sure you access the individual values and not the object */}
             {itemCounts && itemCounts.itemCount !== undefined
               ? itemCounts.itemCount
               : "Loading..."}
