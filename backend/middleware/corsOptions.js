6fe6ca0370216d1e26bdd87f1e630c3d55ca2dc6
Kevin <kevinglennon01@gmail.com>
2025-01-07 00:27:55 +0000

    Updating cor routes for testing and error handling

---
+++ backend/middleware/corsOptions.js
@@ -0,0 +1,13 @@
+const corsOptions = {
+  origin: [
+    "http://localhost:5173",
+    "http://localhost:5174",
+    "http://192.168.0.197:5173",
+    "http://192.168.0.197:5174",
+  ],
+  methods: ["GET", "POST"],
+  allowedHeaders: ["Content-Type"],
+  credentials: true,
+};
+
+module.exports = corsOptions;
--- backend/middleware/errorHandler.js
+++ backend/middleware/errorHandler.js
@@ -1,5 +1,6 @@
 const errorHandler = (err, req, res, next) => {
   console.error(err);
+
   res.status(500).json({
     success: false,
     message: err.message || "An unexpected error occurred",
