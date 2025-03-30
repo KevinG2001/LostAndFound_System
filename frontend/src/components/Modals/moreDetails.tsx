f1d4fc068156e6cac2a3a05e48b02ad708d3126d
Kevin <kevinglennon01@gmail.com>
2025-03-30 14:00:34 +0100

    Ticket page more details

--- frontend/src/components/Modals/moreDetails.tsx
+++ frontend/src/components/Modals/moreDetails.tsx
@@ -192,20 +192,16 @@ const MoreDetailsModal = ({
         <div className={Styles.detailsContainer}>
           <h2 className={Styles.detailsTitle}>Ticket Details</h2>
           <div className={Styles.detailsRow}>
-            <span className={Styles.detailLabel}>ID:</span>
+            <span className={Styles.detailLabel}>Ticket ID:</span>
             <span className={Styles.detailValue}>{data.ticketId}</span>
+            <span className={Styles.detailLabel}>Ticket Status:</span>
+            <span className={Styles.detailValue}>{data.status}</span>
           </div>
           <div className={Styles.detailsRow}>
             <span className={Styles.detailLabel}>Date Lost:</span>
             <span className={Styles.detailValue}>{data.dateLost}</span>
-          </div>
-          <div className={Styles.detailsRow}>
             <span className={Styles.detailLabel}>Date Created:</span>
             <span className={Styles.detailValue}>{data.dateCreated}</span>
-          </div>
-          <div className={Styles.detailsRow}>
-            <span className={Styles.detailLabel}>Status:</span>
-            <span className={Styles.detailValue}>{data.status}</span>
           </div>
           <div className={Styles.chatContainer}>
             <TicketChat
