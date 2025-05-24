568f7525142fad44939c04acdfef79fb4d6e806c
Kevin <kevinglennon01@gmail.com>
2025-05-24 18:32:11 +0100

    Charts updated to MUI

--- frontend/src/components/Charts/DoughtnutChart.tsx
+++ frontend/src/components/Charts/DoughtnutChart.tsx
@@ -1,13 +1,24 @@
+import { Box, Typography } from "@mui/material";
 import { Doughnut } from "react-chartjs-2";
 import { Chart as ChartJS, ArcElement, Tooltip, Legend } from "chart.js";
-import Styles from "../../styles/charts/doughnutStyles.module.scss";
 import { DoughnutChartProps } from "../../util/types/chartTypes";
 
 ChartJS.register(ArcElement, Tooltip, Legend);
 
 const DoughnutChart = ({ data, error }: DoughnutChartProps) => {
   if (error) {
-    return <p>{error}</p>;
+    return (
+      <Box
+        sx={{
+          height: 300,
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      >
+        <Typography color="error">{error}</Typography>
+      </Box>
+    );
   }
 
   const chartData = {
@@ -36,14 +47,41 @@ const DoughnutChart = ({ data, error }: DoughnutChartProps) => {
   };
 
   return (
-    <div className={Styles.chartContainer}>
+    <Box
+      sx={{
+        height: 350,
+        maxWidth: 400,
+        margin: "0 auto",
+        padding: 2,
+        boxSizing: "border-box",
+      }}
+    >
       <Doughnut
         data={chartData}
         options={{
           maintainAspectRatio: false,
+          responsive: true,
+          layout: {
+            padding: 20,
+          },
+          plugins: {
+            legend: {
+              position: "right",
+              labels: {
+                boxWidth: 12,
+                padding: 15,
+                font: {
+                  size: 14,
+                },
+              },
+            },
+            tooltip: {
+              enabled: true,
+            },
+          },
         }}
       />
-    </div>
+    </Box>
   );
 };
 
